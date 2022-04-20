f6de2c77407a0395729f5334c711ae55282203a8
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-04-20 20:54:45 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j==(n-1)||j==n)             {                 continue;             }             printf("%d",j);         }         for(j=(n-3);j>=1;j--)         {             printf("%d",j);         }         printf(" ");     } }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j==(n-1)||j==n)
+            {
+                continue;
+            }
+            printf("%d",j);
+        }
+        for(j=(n-3);j>=1;j--)
+        {
+            printf("%d",j);
+        }
+        printf("
+");
+    }
+}
